1a5cde040ef67b6a9c739159b4e9d1d672b4d9dc
MessasKouseila <k_messas@yahoo.fr>
2017-03-20 15:02:07 +0100

    final test providerMetadata

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -11,18 +11,32 @@ import org.mockito.MockitoAnnotations;
 public final class IPokemonMetadataProviderTest {
 	
 	@Mock private IPokemonMetadataProvider iPokMetaProvider1;
+	
 	private PokemonMetadata Metadata = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
 	private PokemonMetadata Metadata2 = new PokemonMetadata(133, "Aquali", 186, 168, 260);
 	
 	@Test
 	public void getPokemonMetadataTest() throws PokedexException {
-		assertEquals(iPokMetaProvider1.getPokemonMetadata(0), Metadata);
-		assertEquals(iPokMetaProvider1.getPokemonMetadata(133), Metadata2);
+		assertEquals(iPokMetaProvider1.getPokemonMetadata(0).getIndex(), Metadata.getIndex());
+		assertEquals(iPokMetaProvider1.getPokemonMetadata(0).getName(), Metadata.getName());
+		assertEquals(iPokMetaProvider1.getPokemonMetadata(0).getAttack(), Metadata.getAttack());
+		assertEquals(iPokMetaProvider1.getPokemonMetadata(0).getDefense(), Metadata.getDefense());
+		assertEquals(iPokMetaProvider1.getPokemonMetadata(0).getStamina(), Metadata.getStamina());
+		
+		assertEquals(iPokMetaProvider1.getPokemonMetadata(133).getIndex(), Metadata2.getIndex());
+		assertEquals(iPokMetaProvider1.getPokemonMetadata(133).getName(), Metadata2.getName());
+		assertEquals(iPokMetaProvider1.getPokemonMetadata(133).getAttack(), Metadata2.getAttack());
+		assertEquals(iPokMetaProvider1.getPokemonMetadata(133).getDefense(), Metadata2.getDefense());
+		assertEquals(iPokMetaProvider1.getPokemonMetadata(133).getStamina(), Metadata2.getStamina());
 	}
 
 	@Test(expected = PokedexException.class)
 	public void execptionIndex() throws PokedexException {
 		iPokMetaProvider1.getPokemonMetadata(-1);	
+	}
+	@Test(expected = PokedexException.class)
+	public void execptionIndexOut() throws PokedexException {
+		iPokMetaProvider1.getPokemonMetadata(151);	
 	}
 	
 	@Before
@@ -31,6 +45,7 @@ public final class IPokemonMetadataProviderTest {
 		Mockito.when(iPokMetaProvider1.getPokemonMetadata(0)).thenReturn(Metadata);
 		Mockito.when(iPokMetaProvider1.getPokemonMetadata(133)).thenReturn(Metadata2);
 		Mockito.when(iPokMetaProvider1.getPokemonMetadata(-1)).thenThrow(new PokedexException("Index inexistant"));
+		Mockito.when(iPokMetaProvider1.getPokemonMetadata(151)).thenThrow(new PokedexException("Index outOfBounds"));
 	}
 	
 }
